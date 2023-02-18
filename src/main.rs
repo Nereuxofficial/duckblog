a5a05e97a41256b5166601d03434aee377e88d0e
Bene <37740907+Nereuxofficial@users.noreply.github.com>
2023-02-18 14:11:14 +0100

    Switched to Liquid

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod post;
 
 use crate::post::Post;
-use axum::body::{Body, BoxBody};
+use axum::body::Body;
 use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::response::{Html, IntoResponse, Response};
 use axum::{routing::get, Router};
-
 use color_eyre::Result;
-use maud::html;
+use liquid::{object, Object};
 use new_mime_guess::MimeGuess;
+use std::fs::read_to_string;
 use std::net::SocketAddr;
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
@@ -17,18 +17,17 @@ use tracing::*;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // initialize color_eyre
     color_eyre::install()?;
+
     // initialize tracing
     tracing_subscriber::fmt::init();
-    // TODO: Separate Router for posts
-    // build our application with a route
     let app = Router::new()
         .route("/posts/*path", get(get_post))
         .route("/posts/", get(list_posts))
         .fallback(handler_404);
 
     // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     debug!("listening on {}", addr);
     axum::Server::bind(&addr)
@@ -37,6 +36,7 @@ async fn main() -> Result<()> {
         .unwrap();
     Ok(())
 }
+
 async fn get_image(path: String) -> impl IntoResponse {
     debug!("Image `{}` requested", path);
     if let Ok(mut file) = File::open(format!("content/posts/{path}")).await {
@@ -44,6 +44,7 @@ async fn get_image(path: String) -> impl IntoResponse {
         file.read_to_end(&mut buffer)
             .await
             .expect("Could not read image");
+
         // Return the image with the right mime type
         Response::builder()
             .header(
@@ -70,20 +71,27 @@ async fn get_post(Path(path): Path<String>) -> impl IntoResponse {
     }
     debug!("Post `{}` requested", path);
     if let Ok(post) = Post::load(path).await {
-        Html(format!(
-            "{}{}",
-            html! {
-                h1 { (post.metadata.title)}
-                h3 { (post.metadata.description)}
-            }
-            .into_string(),
-            post.content
-        ))
-        .into_response()
+        let compiler = liquid::ParserBuilder::with_stdlib()
+            .build()
+            .expect("Could not build liquid compiler");
+        let template = compiler
+            .parse(&read_to_string("src/post.html.liquid").unwrap())
+            .unwrap();
+        let title = post.metadata.title.clone();
+        let description = post.metadata.description.clone();
+        let content = post.content.clone();
+        let globals: Object = object!({
+            "title": title,
+            "description": description,
+            "content": content
+        });
+        let markup = template.render(&globals).unwrap();
+        Html(markup).into_response()
     } else {
         handler_404().await.into_response()
     }
 }
+
 async fn handler_404() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, "nothing to see here")
 }
