9a55b6deae7ab992a85bc73213277995c50e75cb
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2023-10-13 13:04:13 +0200

    feat: Caching for sponsors

--- src/sponsors.rs
+++ src/sponsors.rs
@@ -1,16 +1,28 @@
+use std::sync::OnceLock;
+use moka::future::Cache;
 use reqwest::{Client, Url};
 use serde::{Deserialize, Serialize};
 use tracing::instrument;
 
-#[derive(Debug, Serialize, Deserialize)]
+pub static SPONSOR_CACHE: OnceLock<Cache<String, Vec<Sponsor>>> = OnceLock::new();
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Sponsor {
     name: String,
     url: Url,
 }
 
 #[instrument(err)]
+/// Gets the sponsors of the blog from GitHub. Result is cached for 1 hour. So worst case this takes around 300ms, which sucks a bit... Maybe some async task so it is always fast?
 pub async fn get_sponsors() -> color_eyre::Result<Vec<Sponsor>> {
-    // Define your GraphQL query
+    let cache = SPONSOR_CACHE.get_or_init(|| {
+        Cache::builder().time_to_live(std::time::Duration::from_secs(60*60)).build()
+    });
+    let sponsors = cache.get_with("GitHub".to_string(), async { noncached_get_sponsors().await.unwrap() }).await;
+    Ok(sponsors)
+}
+
+#[instrument(err)]
+pub async fn noncached_get_sponsors() -> color_eyre::Result<Vec<Sponsor>> {
     let graphql_query = r#"
     {
       viewer {
@@ -43,7 +55,7 @@ pub async fn get_sponsors() -> color_eyre::Result<Vec<Sponsor>> {
             serde_json::json!({
                 "query": graphql_query,
             })
-            .to_string(),
+                .to_string(),
         )
         .send()
         .await?
@@ -82,7 +94,7 @@ mod tests {
     #[tokio::test]
     async fn test_get_sponsors() {
         dotenv().ok();
-        let sponsors = get_sponsors().await.unwrap();
+        let sponsors = noncached_get_sponsors().await.unwrap();
         assert!(!sponsors.is_empty());
     }
 }
